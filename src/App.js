045d9a16c3ba94f0d7d07c438fd7291f25ea4e53
Maksim Ivanov <satansdeer@gmail.com>
2019-06-22 19:24:52 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+  import React from "react"
+  import ReactDOM from "react-dom"
+
+  const App = () => (
+    <>
+      <h1>Hello Webpack</h1>
+      <p>Minimal React configuration.</p>
+    </>
+  )
+
+  ReactDOM.render(<App />, document.getElementById("root"))
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./src/App.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebPackPlugin({ template: "./src/index.html" })]
+};
